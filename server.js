a35d7b0c2a47ea6705c1a75efe3d6f5c7e7765c5
Glitch (alj-devops-santa-app-1) <none>
2020-08-20 13:22:45 +0900

    remove unnecessary

--- server.js
+++ server.js
@@ -10,17 +10,6 @@ const path = require('path');
 const layout = require('express-layout');
 const routes = require('./routes');
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// // http://expressjs.com/en/starter/static-files.html
-// app.use(express.static('public'));
-
-// // http://expressjs.com/en/starter/basic-routing.html
-// app.get('/', (request, response) => {
-//   response.sendFile(__dirname + '/views/index.html');
-// });
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -32,17 +21,7 @@ const middlewares = [
   morgan(),
 ];
 app.use(middlewares);
-
 app.use('/', routes);
-
-app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, function () {
