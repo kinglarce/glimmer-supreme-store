7b40614a5ea0580295d7cfed069ee91e0eb1b150
Glitch (alj-devops-santa-app-1) <none>
2020-08-22 16:22:31 +0900

    Add throwing error for mailer

--- model/Mailer.js
+++ model/Mailer.js
@@ -4,6 +4,15 @@ const TO = 'santa@northpole.com';
 const SENDER_EMAIL = process.env.EMAIL_CLIENT_SENDER_EMAIL;
 const SENDER_PASS = process.env.EMAIL_CLIENT_SENDER_PASSWORD;
 
+const setTransporter = () => nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: SENDER_EMAIL,
+    pass: SENDER_PASS,
+  },
+});
+
 const getBody = (data) => {
   const { username, address, wish } = data;
   return `
@@ -21,16 +30,11 @@ const send = (data) => {
   const correlationId = getRandomId();
   try {
     console.log(`Correlation ID ${correlationId}: Initializing email sender`);
+    if (!SENDER_EMAIL || !SENDER_PASS) throw new Error('Invalid credentials.');
+
     const { username } = data;
     const subject = `A wish from ${username}`;
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      auth: {
-        user: SENDER_EMAIL,
-        pass: SENDER_PASS,
-      },
-    });
+    const transporter = setTransporter();
 
     const message = {
       from: `Cute Kid <${FROM}>`,
@@ -39,10 +43,11 @@ const send = (data) => {
       text: subject,
       html: getBody(data),
     };
+
     console.log(`Correlation ID ${correlationId}: Sending email`);
     transporter.sendMail(message, () => {});
     console.log(`Correlation ID ${correlationId}: Successfully sent email for user ${username}`);
-  } catch (e) {
+  } catch (error) {
     console.error(`Correlation ID ${correlationId}: ${error}`);
   }
 };
